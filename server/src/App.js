f8068a736d8d6d42a87d8429602a1fdd2547814b
Mukilan Palanichamy <mukilan.p@kalvium.community>
2024-10-10 10:05:16 +0530

    Removed secret from GitHubProfile.jsx

---
+++ server/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Portfolio from './components/Portfolio'; 
+// import GitHubProfile from './components/GitHubProfile';
+
+function App() {
+    return (
+        <div>
+            <Portfolio />  
+            {/* <GitHubProfile/> */}
+        </div>
+    );
+}
+
+export default App;
